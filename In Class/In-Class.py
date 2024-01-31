e6b2fec53e732bd24f16752fc88a8b775a463432
CFL429BC <londal@bc.edu>
2024-01-31 11:17:25 -0500

    elif

--- In Class/In-Class.py
+++ In Class/In-Class.py
@@ -22,15 +22,21 @@
 # else:
 #     print("womp womp")
 
+
+
 # int(input("integer input: ")) #whole numbers, pos or neg
 # float(input("float input: ")) #number including decimals, pos or neg
 # str(input("string input: ")) #creates a string
+
+
 
 # 5**2 #5 raised to the 2 power
 # 5//2 #5 divided by 2 with no remainder (instead of 2.5 will get 2)
 # 5%2 #remainder of 5 divided by 2 (will get 1 because 2 goes into 5 twice and leaves 1)
 # "first" + "second" #gives you firstsecond (smushes them together)
 # "first" * 2 #gives you firstfirst (repeats string)
+
+
 
 # num1 = float(input("First number: "))
 # num2 = float(input("Second number: "))
@@ -39,3 +45,15 @@
 # print(f"The average of {num1}, {num2}, and {num3} is {average}")
 
 # num = float(input(f"Number: ")
+
+
+
+exam1 = float(input("Score of exam 1: "))
+exam2 = float(input("Score of exam 2: "))
+
+if exam1 > 90 and exam2 > 90:
+    print("Way to go!")
+elif exam1 < 70 or exam2 < 70:
+    print("Study more!")
+else:
+    print("Keep it up!")
