25fa64d16fa6d6ffff0dc952c19f0592f73a6a32
CFL429BC <londal@bc.edu>
2024-02-12 17:34:16 -0500

    d5p1

---
+++ Discussion/d5p1.py
@@ -0,0 +1,28 @@
+# atm problem
+
+import sys
+
+balance = 1000
+func = str("start")
+print("Welcome to your friendly ATM!")
+while func != "quit":
+    print(f"Current balance: ${balance:.2f}")
+    func = str(input("What you want to do (deposit, withdraw, quit)? "))
+    if func == "deposit":
+        amt = float(input("How much? $"))
+        if amt < 0:
+            print("Can't deposit negative amounts!")
+        else:
+            balance += amt
+    elif func == "withdraw":
+        amt = float(input("How much? $"))
+        if amt < 0:
+            print("Can't withdraw negative amounts!")
+        elif amt > balance:
+            print("Can't withdraw more than you have!")
+        else:
+            balance -= amt
+    elif func != "quit":
+        print("Invalid operation!")
+
+print(f"Final balance: ${balance:.2f}")
