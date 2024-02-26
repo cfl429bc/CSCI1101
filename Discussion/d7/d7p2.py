056c17a0333dfa11dd6009a82f8c0250425f8cdd
CFL429BC <londal@bc.edu>
2024-02-26 18:14:49 -0500

    Problem 2

--- Discussion/d7/d7p2.py
+++ Discussion/d7/d7p2.py
@@ -1,40 +1,27 @@
 import sys
 
 def posinput(string):
-    num = int(input(f"Enter {string}: "))
+    num = int(input(f"Enter a value for {string}: "))
     if num <= 0:
         print(f"{string} must be positive!")
         sys.exit()
     return num
 
-# def factortest(num, n):
-#     zero = 0
-#     remain = num % n
-#     if remain == 0:
-#         return n
-#     else:
-#         return zero
+def factortest(num, n): # outputs the factors, or 0 if not a factor
+    zero = 0
+    remain = num % n
+    if remain == 0:
+        return n
+    else:
+        return zero
 
 N = posinput("N")
 
 sum = 0
 for i in range(1, N):
-    remain = N % i
-    if remain == 0:
-        sum += i
+    sum += factortest(N, i)
 
 if N == sum:
     print(f"{N} is a perfect number!")
 else:
     print(f"{N} is not a perfect number.")
-
-# othersum = 0
-# for i in range(1, N):
-#     test = factortest(N, i)
-#     othersum += test
-# print(f"othersum = {othersum}")
-    
-# if N == othersum:
-#     print(f"{N} is a perfect number!")
-# else:
-#     print(f"{N} is not a perfect number.")
