9bf089e3f53b58e56bd4adc8ebb6d87ca73c6642
CFL429BC <londal@bc.edu>
2024-03-18 20:31:17 -0400

    update functions

--- In Class/functions.py
+++ In Class/functions.py
@@ -59,15 +59,34 @@
 #     else:
 #         return zero # if this is not included it outputs the string "none"
 
-def f(a, b):
-    result = ""
-    for c in a:
-        if c.isalpha():
-            result += c
-    for c in b:
-        if not c.isalpha():
-            result += c
-    print(result)
+# def f(a, b):
+#     result = ""
+#     for c in a:
+#         if c.isalpha():
+#             result += c
+#     for c in b:
+#         if not c.isalpha():
+#             result += c
+#     print(result)
 
-s = "x=42,right?"
-f(s, s)
+# s = "x=42,right?"
+# f(s, s)
+
+# def merge_lists(list1,list2):
+#     merged = []
+#     len_min = min(len(list1),len(list2))
+#     for i in range(max(len(list1),len(list2))):
+#         if i <= (len_min - 1):
+#             merged.append(list1[i] + list2[i])
+#         else:
+#             if len(list1) > len_min:
+#                 merged.append(list1[i])
+#             if len(list2) > len_min:
+#                 merged.append(list2[i])
+#     return merged
+
+# def check_sorted(list):
+#     for i in range(len(list)-1):
+#             if list[i] > list[i+1]:
+#                 return False
+#     return True
