4983aba34ce9aacd121678ea21369d8256c7437d
CFL429BC <londal@bc.edu>
2024-04-27 17:28:30 -0400

    list comp

--- Testfile.py
+++ Testfile.py
@@ -1,10 +1,15 @@
 def reverse_order(some_list):
-    rev_list = []
-    for i in range(len(some_list)-1, -1, -1):
-        rev_list.append(some_list[i])
-        print(i)
+    rev_list = [some_list[i] for i in range(len(some_list)-1, -1, -1)]
     return rev_list
 values = [1, 2, 3, 4]
 result = reverse_order(values)
 # this will print [4, 3, 2, 1]
 print(result)
+
+# a = [[1,2,3], [4,5,6]]
+# transpose = []
+# for j in range(len(a[0])):
+#     transpose.append([])
+#     for i in range(len(a)):
+#         transpose[j].append(a[i][j])
+# print(transpose)
